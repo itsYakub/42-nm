9febfbe05c7878bf945ac3402872f59030ddef02
Jakub Oleksiak <yakubofficialmail@gmail.com>
2026-01-20 22:41:38 +0100

    Huge refactor
    Codebase for handling files and ELF components was completely rewritten. Now ft_nm can handle both x32 and x64 binaries. Correct symbols are now being logged to stdout. No leaks detected. On the top of ft_nm.c there's a new TODO of the tasks

--- ft_nm.c
+++ ft_nm.c
@@ -1,3 +1,17 @@
+/* TODO:
+ * 1. Store the list of symbols to be printed as a seperate pointer.
+ *    We can, technically speaking, memdup the whole symbol table of .symtab section and that should work fine.
+ * 2. Implement sort and reverse sort for symbols
+ * 3. Find out a type of each symbol based on requirements from man
+ * 4. Get rid of printf in favor of something else (could be the simplest libft or ft_printf because it handles most of the cases pretty well)
+ * 4. Implement proper getopt
+ * 5. Implement -a flag
+ * 6. Implement -g flag
+ * 7. Implement -u flag
+ * 8. Implement -r flag
+ * 9. Implement -p flag
+ * */
+
 #include <stdio.h>      /* perror() */
 #include <stdlib.h>     /* malloc(), free(), exit()  */
 
@@ -13,36 +27,19 @@
 
 #include "libft/libft.h"
 
-/* SECTION: preprocessor
- * */
-
-#if defined (__LP64__)
-# define ElfEhdr    Elf64_Ehdr
-# define ElfShdr    Elf64_Shdr
-# define ElfPhdr    Elf64_Phdr
-# define ElfSym     Elf64_Sym
-#else
-# define ElfEhdr    Elf32_Ehdr
-# define ElfShdr    Elf32_Shdr
-# define ElfPhdr    Elf32_Phdr
-# define ElfSym     Elf32_Sym
-#endif /* __LP64__ */
-
 /* SECTION: api
  * */
 
 extern int ft_getopt(int, char **);
-extern int ft_procFile(const char *);
-extern int ft_procELF(const char *, const char *);
+extern int ft_file(const char *);
 
-extern ElfEhdr *ft_ElfEhdrLoad(const char *, const char *);
-extern ElfPhdr *ft_ElfPhdrLoad(ElfEhdr *, const char *);
-extern ElfShdr *ft_ElfShdrLoad(ElfEhdr *, const char *);
+extern int ft_elf32(const char *);
 
-extern ElfSym *ft_ElfSymLoad(const char *, const size_t, const size_t);
-extern ElfSym *ft_ElfGetStrtab(ElfEhdr *, ElfShdr *, const char *);
+extern int ft_elf64(const char *);
 
-extern int ft_ElfUnload(void *);
+extern int ft_elf_getMagic(const char *);
+extern int ft_elf_getArch(const char *);
+extern void *ft_elf_extract(const char *, const size_t, const size_t);
 
 /* SECTION: globals
  * */
@@ -67,7 +64,7 @@ int main(int ac, char **av) {
 
         if (!name) { return (1); }
         if (ft_lstsize(g_paths) > 1) { printf("\n%s:\n", name); }
-        if (!ft_procFile(name)) { break; }
+        if (!ft_file(name)) { break; }
     }
 
     ft_lstclear(&g_paths, free), g_paths = 0;
@@ -98,7 +95,7 @@ int ft_getopt(int ac, char **av) {
     return (1);
 }
 
-int ft_procFile(const char *path) {
+int ft_file(const char *path) {
     /* setup... */
     int fd = open(path, O_RDONLY);
     if (fd == -1) {
@@ -133,9 +130,14 @@ int ft_procFile(const char *path) {
     close(fd), fd = 0;
     
     /* execution... */
-    if (!ft_procELF(path, buffer)) {
-        free(buffer), buffer = 0;
-        return (0);
+    if (ft_elf_getMagic(buffer)) {
+        switch (ft_elf_getArch(buffer)) {
+            case (ELFCLASS32): { ft_elf32(buffer); } break;
+            case (ELFCLASS64): { ft_elf64(buffer); } break;
+        }
+    }
+    else {
+        printf("%s: %s: file format not recognized\n", g_prog, path);
     }
     
     /* cleanup... */
@@ -144,200 +146,146 @@ int ft_procFile(const char *path) {
     return (1);
 }
 
-int ft_procELF(const char *path, const char *buffer) {
+extern int ft_elf32(const char *buffer) {
+    (void) buffer;
+    return (0);
+}
+
+extern int ft_elf64(const char *buffer) {
     int exitcode = 1;
 
     /* ehdr... */
-    ElfEhdr *ehdr = ft_ElfEhdrLoad(path, buffer);
+    Elf64_Ehdr *ehdr = ft_elf_extract(buffer, sizeof(Elf64_Ehdr), 0);
     if (!ehdr) {
-        exitcode = 0; goto exit0;
-    }
-
-    /* phdr table... */
-    ElfPhdr *phdr_tb = ft_ElfPhdrLoad(ehdr, buffer);
-    if (!phdr_tb) {
-        exitcode = 0; goto exit0;
+        exitcode = 0; goto ft_elf64_exit;
     }
 
     /* shdr table... */
-    ElfShdr *shdr_tb = ft_ElfShdrLoad(ehdr, buffer);
+    Elf64_Shdr *shdr_tb = ft_elf_extract(buffer, ehdr->e_shnum * ehdr->e_shentsize, ehdr->e_shoff);
     if (!shdr_tb) {
-        exitcode = 0; goto exit0;
+        exitcode = 0; goto ft_elf64_exit;
     }
 
-    /* .strtab ... */
-    ElfSym *strtab = ft_ElfGetStrtab(ehdr, shdr_tb, buffer);
-    if (!strtab) {
-        exitcode = 0; goto exit0;
-    }
+    /* extract STRTAB's... */
+    const char *shstrtab = ft_elf_extract(buffer, shdr_tb[ehdr->e_shstrndx].sh_size, shdr_tb[ehdr->e_shstrndx].sh_offset);
+    const char *dynstr = 0;
+    const char *strtab = 0;
+    for (size_t i = 0; i < ehdr->e_shnum; i++) {
+        /* section header object... */
+        Elf64_Shdr shdr = shdr_tb[i];
 
-    /* phdr print... */
-    for (size_t i = 0; i < ehdr->e_phnum; i++) {
-        ElfPhdr phdr   = phdr_tb[i];
-        printf("phdr:\n");
-        printf("- type: 0x%x\n", phdr.p_type);
-        
-        ElfSym *sym_tb = ft_ElfSymLoad(buffer, phdr.p_filesz, phdr.p_offset);
-        if (!sym_tb) {
-            exitcode = 0; goto exit0;
+        if (!strcmp(shstrtab + shdr.sh_name, ".dynstr")) {
+            dynstr = ft_elf_extract(buffer, shdr.sh_size, shdr.sh_offset);
+            if (!dynstr) {
+                exitcode = 0; goto ft_elf64_exit;
+            }
         }
 
-        for (size_t j = 0; j < phdr.p_filesz / sizeof(ElfSym); j++) {
-            ElfSym sym = sym_tb[j];
-
-            /* ... */
-            printf("\tsymbol:\n");
-            printf("\t- name:  0x%x\n", sym.st_name);
-            printf("\t- value: 0x%lx\n", sym.st_value);
-            printf("\t- size:  0x%lx\n", sym.st_size);
-        } 
-        
-        ft_ElfUnload(sym_tb), sym_tb = 0;
+        else if (!strcmp(shstrtab + shdr.sh_name, ".strtab")) {
+            strtab = ft_elf_extract(buffer, shdr.sh_size, shdr.sh_offset);
+            if (!strtab) {
+                exitcode = 0; goto ft_elf64_exit;
+            }
+        }
     }
 
     /* shdr print... */
     for (size_t i = 0; i < ehdr->e_shnum; i++) {
-        ElfShdr shdr   = shdr_tb[i];
-        printf("shdr:\n");
-        printf("- type: 0x%x\n", shdr.sh_type);
-        printf("- name: %u\n", shdr.sh_name);
-        
-        ElfSym *sym_tb = ft_ElfSymLoad(buffer, shdr.sh_size, shdr.sh_offset);
+        Elf64_Shdr shdr = shdr_tb[i];
+        if (shdr.sh_type != SHT_SYMTAB) {
+            continue;
+        }
+
+        /* select current string table... */
+        const char *string_tb = 0;
+        if (shdr.sh_type == SHT_SYMTAB) {
+            string_tb = strtab;
+        }
+
+        Elf64_Sym *sym_tb = ft_elf_extract(buffer, shdr.sh_size, shdr.sh_offset);
         if (!sym_tb) {
-            exitcode = 0; goto exit0;
+            exitcode = 0; goto ft_elf64_exit;
         }
 
-        for (size_t j = 0; j < shdr.sh_size / sizeof(ElfSym); j++) {
-            ElfSym sym = sym_tb[j];
+        for (size_t j = 0; j < shdr.sh_size / sizeof(Elf64_Sym); j++) {
+            Elf64_Sym sym = sym_tb[j];
+            
+            const uint8_t st_type = ELF64_ST_TYPE(sym.st_info);
+            if (st_type != STT_NOTYPE &&
+                st_type != STT_OBJECT &&
+                st_type != STT_FUNC &&
+                st_type != STT_COMMON
+            ) {
+                continue;
+            }
 
-            /* ... */
-            printf("\tsymbol:\n");
-            printf("\t- name:  0x%x\n", sym.st_name);
-            printf("\t- value: 0x%lx\n", sym.st_value);
-            printf("\t- size:  0x%lx\n", sym.st_size);
+            const char *st_name = string_tb + sym.st_name;
+            if (!st_name || !st_name[0]) {
+                continue;
+            }
+
+            /* print address... */
+            if (sym.st_value) { printf("%016lx ", sym.st_value); }
+            else { printf("%*c ", 16, ' '); }
+            
+            /* print type... */
+            printf("A ");
+
+            /* print name... */
+            printf("%s\n", st_name);
         }
 
-        ft_ElfUnload(sym_tb), sym_tb = 0;
+        free(sym_tb), sym_tb = 0;
     }
 
-exit0:
-    if (strtab)  { ft_ElfUnload(strtab),  strtab = 0; }
-    if (shdr_tb) { ft_ElfUnload(shdr_tb), shdr_tb = 0; }
-    if (phdr_tb) { ft_ElfUnload(phdr_tb), phdr_tb = 0; }
-    if (ehdr)    { ft_ElfUnload(ehdr),    ehdr = 0; }
+ft_elf64_exit:
+    if (shstrtab) { free((void *) shstrtab), shstrtab = 0; }
+    if (strtab)   { free((void *) strtab), strtab = 0; }
+    if (dynstr)   { free((void *) dynstr), dynstr = 0; }
+    if (shdr_tb)  { free(shdr_tb), shdr_tb = 0; }
+    if (ehdr)     { free(ehdr), ehdr = 0; }
     return (exitcode);
 }
 
-ElfEhdr *ft_ElfEhdrLoad(const char *path, const char *buffer) {
-    /* safety check... */
-    if (!path) { return (0); }
+int ft_elf_getMagic(const char *buffer) {
+    /* null-check... */
     if (!buffer) { return (0); }
 
-    /* allocate ehdr... */
-    ElfEhdr *ehdr = malloc(sizeof(ElfEhdr));
-    if (!ehdr) { return (0); }
-    if (!ft_memcpy((void *) ehdr, buffer, sizeof(ElfEhdr))) {
-        free(ehdr);
-        return (0);
-    } 
-
-    /* check magic... */
-    if (ft_memcmp(ehdr->e_ident, ELFMAG, SELFMAG)) {
-        printf("%s: '%s': Not an ELF\n", g_prog, path);
-        ft_ElfUnload(ehdr);
+    if (buffer[EI_MAG0] != ELFMAG0 ||
+        buffer[EI_MAG1] != ELFMAG1 ||
+        buffer[EI_MAG2] != ELFMAG2 ||
+        buffer[EI_MAG3] != ELFMAG3
+    ) {
         return (0);
     }
-
-    return (ehdr);
-}
-
-extern ElfPhdr *ft_ElfPhdrLoad(ElfEhdr *ehdr, const char *buffer) {
-    /* safety check... */
-    if (!ehdr) { return (0); }
-    if (!buffer) { return (0); }
-
-    /* helper variables... */
-    size_t phoff = ehdr->e_phoff;
-    size_t phnum = ehdr->e_phnum;
-    size_t phentsize = ehdr->e_phentsize;
-    const char *phbuf = buffer + phoff;
-
-    /* allocate phdr... */
-    ElfPhdr *phdr = malloc(phentsize * phnum);
-    if (!phdr) { return (0); }
-    if (!ft_memcpy((void *) phdr, phbuf, phnum * phentsize)) {
-        free(phdr), phdr = 0;
-        return (0);
-    }
-
-    return (phdr);
-}
-
-ElfShdr *ft_ElfShdrLoad(ElfEhdr *ehdr, const char *buffer) {
-    /* safety check... */
-    if (!ehdr) { return (0); }
-    if (!buffer) { return (0); }
-
-    /* helper variables... */
-    size_t shoff = ehdr->e_shoff;
-    size_t shnum = ehdr->e_shnum;
-    size_t shentsize = ehdr->e_shentsize;
-    const char *shbuf = buffer + shoff;
-
-    /* allocate shdr... */
-    ElfShdr *shdr = malloc(shentsize * shnum);
-    if (!shdr) { return (0); }
-    if (!ft_memcpy((void *) shdr, shbuf, shnum * shentsize)) {
-        free(shdr), shdr = 0;
-        return (0);
-    }
-
-    return (shdr);
-}
-
-ElfSym *ft_ElfSymLoad(const char *buffer, const size_t size, const size_t offset) {
-    /* safety check... */
-    if (!buffer) { return (0); }
-
-    /* allocate sym... */
-    ElfSym *sym = malloc(size);
-    if (!sym) { return (0); }
-    if (!ft_memcpy((void *) sym, buffer + offset, size)) {
-        free(sym), sym = 0;
-        return (0);
-    }
-
-    return (sym);
-}
-
-ElfSym *ft_ElfGetStrtab(ElfEhdr *ehdr, ElfShdr *shdr, const char *buffer) {
-    /* safety check... */
-    if (!ehdr) { return (0); }
-    if (!shdr) { return (0); }
-    if (!buffer) { return (0); }
-
-    /* get .strtab index... */
-    size_t shstrndx = 0;
-    if (ehdr->e_shstrndx != SHN_XINDEX) {
-        shstrndx = ehdr->e_shstrndx;
-    }
-    else {
-        /* process SH_XINDEX... */
-    }
-
-    ElfShdr section = shdr[shstrndx];
-    if (section.sh_type == SHT_NULL) {
-        return (0);
-    }
-
-    return (ft_ElfSymLoad(buffer, section.sh_size, section.sh_offset));
-}
-
-int ft_ElfUnload(void *data) {
-    if (!data) { return (0); }
-    
-    free(data);
     return (1);
+}
+
+int ft_elf_getArch(const char *buffer) {
+    /* null-check... */
+    if (!buffer) { return (0); }
+
+    return (buffer[EI_CLASS]);
+}
+
+void *ft_elf_extract(const char *buffer, const size_t size, const size_t offset) {
+    /* null-check... */
+    if (!buffer) { return (0); }
+
+    /* ...alloc... */
+    void *result = malloc(size);
+    if (!result) {
+        return (0);
+    }
+
+    /* ...copy... */
+    if (!memcpy(result, buffer + offset, size)) {
+        free(result);
+        return (0);
+    }
+
+    /* ...return */
+    return (result);
 }
 
 
